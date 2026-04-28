3fc9392a21ba86ab815c81c7569fad33344d1e1d
Nicole Bobier <ncbobier@stud.ntnu.no>
2026-04-28 18:29:12 +0200

    la til board i app

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,15 @@ import "./App.css";
 import questionsData from './questions.json';
 // questionsData.categories er nå en liste med alle kategoriene
 
+import Board from './components/Board';
+
 function App() {
-  return (
-    <div className="card">
-      <h1>Heyyy❤️ </h1>
-      <p>
-        Du kan se koden for dette prosjektet{" "}
-        <a href="https://github.com/bekk/prosjektutgangspunkt">
-          i repoet på GitHub
-        </a>
-      </p>
-      <p>
-        Du kan endre koden i <code>src/App.jsx</code>
-      </p>
-    </div>
-  );
+    return (
+        <div className="game-screen">
+            <h1 className="game-title">🎯 Jeopardy</h1>
+            <Board />
+        </div>
+    );
 }
 
 export default App;
