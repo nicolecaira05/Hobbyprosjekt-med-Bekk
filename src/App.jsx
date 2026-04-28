11a95794473e596a539eca721bc4497116301e0c
Nicole Bobier <ncbobier@stud.ntnu.no>
2026-04-28 18:30:45 +0200

    fikset app

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import questionsData from './questions.json';
 import Board from './components/Board';
 
 function App() {
-    return (
-        <div className="game-screen">
-            <h1 className="game-title">🎯 Jeopardy</h1>
-            <Board />
-        </div>
-    );
+    <Board/>
 }
 
 export default App;
