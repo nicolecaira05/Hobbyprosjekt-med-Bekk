1888f90315fb6ba8447159a0d68dcbc5a006a0bf
Nicole Bobier <ncbobier@stud.ntnu.no>
2026-04-28 18:34:07 +0200

    endret app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import './App.css';
 import Board from './components/Board';
 
 function App() {
-    return (
-        <div className="game-screen">
-            <h1 className="game-title">🎯 Jeopardy</h1>
-            <Board />
-        </div>
-    );
+    return <Board/>
 }
 
 export default App;
